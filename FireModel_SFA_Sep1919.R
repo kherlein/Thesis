e75877ebb9c806d1a58c51cd60624eb0f2384b29
kherlein <55412855+kherlein@users.noreply.github.com>
2019-09-25 16:20:19 -0600

    Isolated data manipulation code from Frontier Analysis code

--- FireModel_SFA_Sep1919.R
+++ FireModel_SFA_Sep1919.R
@@ -1,10 +1,8 @@
 #ImportData
-setwd("C:/Users/Kalli/OneDrive - ualberta.ca/Grad School/Thesis/FIRE Data/")
-
-gis.data <- read.csv(file="GOV_DATA/R_SFA_Data_Sep24.csv", header=TRUE, sep=",")
+gis.data <- read.csv(file="/R_SFA_Data_Sep24.csv", header=TRUE, sep=",")
 colnames(gis.data)[colnames(gis.data)=="CAPTURE_DA"] <- "weather_date"
-all.weather <- read.csv(file="GOV_DATA/WEATHERDATA_GOV/20174stations.csv")
-operations <- read.csv(file="GOV_DATA/FakeCrewData.csv")
+all.weather <- read.csv(file="20174stations.csv")
+operations <- read.csv(file="FakeCrewData.csv")
 
 # read_xlsx("GOV_DATA/WEATHERDATA_GOV/New Fires Weather and Status 2017.xlsx", "SWF107-PannyAuto")
 
@@ -12,7 +10,7 @@ operations <- read.csv(file="GOV_DATA/FakeCrewData.csv")
 #HWF221_LambertAuto <- read_xlsx("GOV_DATA/WEATHERDATA_GOV/New Fires Weather and Status 2017.xlsx", "HWF221-LambertAuto")
 #HWF236_LambertAuto <- read_xlsx("GOV_DATA/WEATHERDATA_GOV/New Fires Weather and Status 2017.xlsx", "HWF236-LambertAuto")
 #HWF280_BitschoLakeAuto <- read_xlsx("GOV_DATA/WEATHERDATA_GOV/New Fires Weather and Status 2017.xlsx", "HWF280-BitschoLakeAuto")
-ma
+
 
 require(dplyr)
 require(lubridate)
@@ -44,67 +42,4 @@ join.gis.weather.ops <- left_join(join.gis.weather, operations.date, by = c("Fir
 join.gis.weather.ops.nozero <- join.gis.weather.ops %>% 
   mutate(Daily_Held_noneg = ifelse(Daily_Held_noneg == 0,1,Daily_Held_noneg))
 
-
-
-require(sfa)
-require(plm)
-
-# Cobb-Douglas production frontier
-cobbDouglas <- sfa( log( Daily_Held_noneg ) ~ log( Crew ) + log( Helicopter ) + log( Air ) + log( Engine ) + log( Dozer ),
-                    data = join.gis.weather.ops.nozero )
-summary( cobbDouglas )
-
-
-# Error Components Frontier (Battese & Coelli 1992)
-# with observation-specific efficiencies (ignoring the panel structure)
-fire <- sfa( log( Daily_Held_noneg ) ~ log( Crew ) + log( Helicopter ) + log( Air ) + log( Engine ) + log( Dozer ),
-             data = join.gis.weather.ops.nozero )
-summary( fire )
-
-# Error Components Frontier (Battese & Coelli 1992)
-# with "true" fixed individual effects and observation-specific efficiencies
-#fireTrue <- sfa( log( PROD ) ~ log( AREA ) + log( LABOR ) + log( NPK ) + 
-#                   factor( FireID ),  data = join.gis.weather.ops.nozero )
-#summary( fireTrue )
-
-# add data set with information about its panel structure
-library( "plm" )
-firePanel <- pdata.frame( join.gis.weather.ops.nozero, c( "FireID", "FireDayActual.x" ) )
-
-# Error Components Frontier (Battese & Coelli 1992)
-# with time-invariant efficiencies
-fireTimeInv <- sfa( log( Daily_Held_noneg ) ~ log( Crew ) + log( Helicopter ) + log( Air ) + log( Engine ) + log( Dozer ),
-                    data = firePanel )
-summary( fireTimeInv )
-efficiencies( fireTimeInv )
-
-
-# Error Components Frontier (Battese & Coelli 1992)
-# with time-variant efficiencies
-fireTimeVar <- sfa( log( Daily_Held_noneg ) ~ log( Crew ) + log( Helicopter ) + log( Air ) + log( Engine ) + log( Dozer ),
-                    data = firePanel, timeEffect = TRUE )
-summary( fireTimeVar )
-efficiencies( fireTimeVar )
-
-# Technical Efficiency Effects Frontier (Battese & Coelli 1995)
-# (efficiency effects model with intercept)
-fireZ <- sfa( log( Daily_Held_noneg ) ~ log( Crew ) + log( Helicopter ) + log( Air ) + log( Engine ) + log( Dozer ) |
-                EDYRS + BANRAT, data = fireProdPhil )
-summary( fireZ )
-efficiencies( fireZ )
-
-
-# Technical Efficiency Effects Frontier (Battese & Coelli 1995)
-# (efficiency effects model without intercept)
-fireZ2 <- sfa( log( Daily_Held_noneg ) ~ log( Crew ) + log( Helicopter ) + log( Air ) + log( Engine ) + log( Dozer ) |
-                 EDYRS + BANRAT - 1, data = fireProdPhil )
-summary( fireZ2 )
-efficiencies( fireZ2 )
-
-
-# Cost Frontier (with land as quasi-fixed input)
-fireProdPhil$cost <- fireProdPhil$LABOR * fireProdPhil$LABORP +
-  fireProdPhil$NPK * fireProdPhil$NPKP
-fireCost <- sfa( log( cost ) ~ log( PROD ) + log( AREA ) + log( LABORP )
-                 + log( NPKP ), data = fireProdPhil, ineffDecrease = FALSE )
-summary( fireCost )
+#Continue to Frontier_R_Code for analysis code
---
+++ Frontier_R_Code.R
@@ -0,0 +1,64 @@
+
+
+require(frontier)
+require(plm)
+
+# Cobb-Douglas production frontier
+cobbDouglas <- sfa( log( Daily_Held_noneg ) ~ log( Crew ) + log( Helicopter ) + log( Air ) + log( Engine ) + log( Dozer ),
+                    data = join.gis.weather.ops.nozero )
+summary( cobbDouglas )
+
+
+# Error Components Frontier (Battese & Coelli 1992)
+# with observation-specific efficiencies (ignoring the panel structure)
+fire <- sfa( log( Daily_Held_noneg ) ~ log( Crew ) + log( Helicopter ) + log( Air ) + log( Engine ) + log( Dozer ),
+             data = join.gis.weather.ops.nozero )
+summary( fire )
+
+# Error Components Frontier (Battese & Coelli 1992)
+# with "true" fixed individual effects and observation-specific efficiencies
+#fireTrue <- sfa( log( PROD ) ~ log( AREA ) + log( LABOR ) + log( NPK ) + 
+#                   factor( FireID ),  data = join.gis.weather.ops.nozero )
+#summary( fireTrue )
+
+# add data set with information about its panel structure
+library( "plm" )
+firePanel <- pdata.frame( join.gis.weather.ops.nozero, c( "FireID", "FireDayActual.x" ) )
+
+# Error Components Frontier (Battese & Coelli 1992)
+# with time-invariant efficiencies
+fireTimeInv <- sfa( log( Daily_Held_noneg ) ~ log( Crew ) + log( Helicopter ) + log( Air ) + log( Engine ) + log( Dozer ),
+                    data = firePanel )
+summary( fireTimeInv )
+efficiencies( fireTimeInv )
+
+
+# Error Components Frontier (Battese & Coelli 1992)
+# with time-variant efficiencies
+fireTimeVar <- sfa( log( Daily_Held_noneg ) ~ log( Crew ) + log( Helicopter ) + log( Air ) + log( Engine ) + log( Dozer ),
+                    data = firePanel, timeEffect = TRUE )
+summary( fireTimeVar )
+efficiencies( fireTimeVar )
+
+# Technical Efficiency Effects Frontier (Battese & Coelli 1995)
+# (efficiency effects model with intercept)
+fireZ <- sfa( log( Daily_Held_noneg ) ~ log( Crew ) + log( Helicopter ) + log( Air ) + log( Engine ) + log( Dozer ) |
+                EDYRS + BANRAT, data = fireProdPhil )
+summary( fireZ )
+efficiencies( fireZ )
+
+
+# Technical Efficiency Effects Frontier (Battese & Coelli 1995)
+# (efficiency effects model without intercept)
+fireZ2 <- sfa( log( Daily_Held_noneg ) ~ log( Crew ) + log( Helicopter ) + log( Air ) + log( Engine ) + log( Dozer ) |
+                 EDYRS + BANRAT - 1, data = fireProdPhil )
+summary( fireZ2 )
+efficiencies( fireZ2 )
+
+
+# Cost Frontier (with land as quasi-fixed input)
+fireProdPhil$cost <- fireProdPhil$LABOR * fireProdPhil$LABORP +
+  fireProdPhil$NPK * fireProdPhil$NPKP
+fireCost <- sfa( log( cost ) ~ log( PROD ) + log( AREA ) + log( LABORP )
+                 + log( NPKP ), data = fireProdPhil, ineffDecrease = FALSE )
+summary( fireCost )
