3dea81966f669e5be0d84bc66b917255b13055ae
kherlein <55412855+kherlein@users.noreply.github.com>
2019-10-29 16:53:37 -0600

    adding RTS test

--- DEA_RCode.R
+++ DEA_RCode.R
@@ -1,7 +1,28 @@
 require(rDEA)
 
 ## load data
+#######      IGNORING PANEL STRUCTURE     ############################
+######################################################################
+######################################################################
 
+Ycross = join.gis.weather.ops.nozero[c('Daily_Held_noneg')]
+Xcross = join.gis.weather.ops.nozero[c('Crew', 'Helicopter', 'Air', 'Engine', 'Dozer')]
+#W = firePanel[c('labor_price', 'capital_price')]
+
+## Input-oriented DEA score under variable returns-to-scale
+di = dea(XREF=Xcross, YREF=Ycross, X=Xcross, Y=Ycross, model="output", RTS="variable")
+di$thetaOpt
+di
+
+#test for returns to scale
+#not rejecting null indicates RTS are constant
+# need to check which bandwidtch to use as smoothing parameter, cv ,bw.ucv, silverman, or bc.nrd0
+rts.test(X, Y, W=NULL, model = "output", "constant", "cv", 100, 0.05)
+
+
+#######      PANEL STRUCTURE     #####################################
+######################################################################
+######################################################################
 ## inputs and outputs for analysis
 Ypanel = firePanel[c('Daily_Held_noneg')]
 Xpanel = firePanel[c('Crew', 'Helicopter', 'Air', 'Engine', 'Dozer')]
@@ -12,14 +33,7 @@ Xpanel = firePanel[c('Crew', 'Helicopter', 'Air', 'Engine', 'Dozer')]
 di_panel = dea(XREF=Xpanel, YREF=Ypanel, X=Xpanel, Y=Ypanel, model="output", RTS="variable")
 di_panel$thetaOpt
 
-## inputs and outputs for analysis
-Y = join.gis.weather.ops.nozero[c('Daily_Held_noneg')]
-X = join.gis.weather.ops.nozero[c('Crew', 'Helicopter', 'Air', 'Engine', 'Dozer')]
 
-## Naive input-oriented DEA score for the first 20 firms under variable returns-to-scale
-#firms=1:6
-di = dea(XREF=X, YREF=Y, X=X, Y=Y, model="output", RTS="variable")
-di$thetaOpt
 
 ## Naive DEA score in cost-minimization model for the first 20 firms under variable returns-to-scale
 #ci_naive = dea(XREF=X, YREF=Y, X=X[firms,], Y=Y[firms,], W=W[firms,],
