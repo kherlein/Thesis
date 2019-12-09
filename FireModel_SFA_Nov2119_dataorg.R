64863f7c7311c2de71c2d16b1cffe23555ba0a94
kherlein <55412855+kherlein@users.noreply.github.com>
2019-12-09 11:16:29 -0700

    changing inefficiency variables adding correlation plot

--- FireModel_SFA_Nov2119_dataorg.R
+++ FireModel_SFA_Nov2119_dataorg.R
@@ -26,29 +26,29 @@ require(tidyr)
 weather <- all.weather %>% 
   mutate(weather_date=as.Date(weather_date, format = "%Y-%m-%d %H:%M")) %>% 
   group_by(FireID, weather_date) %>% 
-  summarize(RH_max = max(relative_humidity), temp_max = max(dry_bulb_temperature), wind_max = max(wind_gust_kmh, na.rm = TRUE ), agric_wind_max = max(wind_agric)) %>%
+  summarize(RH_max = max(relative_humidity), temp_max = max(dry_bulb_temperature),
+            wind_max = max(wind_gust_kmh, na.rm = TRUE ), wind_speed_max=max(wind_speed_kmh),
+            wind_speed = mean(wind_speed_kmh), agric_wind_max = max(wind_agric),agric_wind_avg = mean(wind_agric),
+            dewpoint_max = max(dew_point), rain_total = sum(rain_mm)) %>%
   as.data.frame()
 
-rh.lag <- all.weather %>% 
+rh.max <- all.weather %>% 
   mutate(weather_date=as.Date(weather_date, format = "%Y-%m-%d %H:%M")) %>% 
   group_by(FireID, weather_date) %>% 
-  summarize(RH_lag = max(relative_humidity)) %>%
+  summarize(rh_max = max(relative_humidity), rain_total = sum(rain_mm) ) %>%
   as.data.frame()
-
 require( "plm" )
-rh.panel <- pdata.frame(rh.lag, c("FireID", "weather_date"))
-
-rh.panel.lag <- lag(rh.panel$RH_lag, k=1, shift="time") %>%  
-   left_join(rh.panel, rh.lag) %>% 
-   as.data.frame()
+rh.panel <- pdata.frame(rh.max, c("FireID", "weather_date"))
+rh.lag <- rh.panel %>% 
+  mutate(rh_lag=lag(rh.panel$rh_max, k=1, shift="time"))  %>% 
+  mutate(rain_lag=lag(rh.panel$rain_total, k=1, shift="time"))  %>% 
+  mutate(weather_date=as.Date(weather_date, format = "%Y-%m-%d")) %>% 
+  as.data.frame()
 
 gis.data.date <- gis.data %>%
   mutate(weather_date=as.Date(weather_date, format = "%Y-%m-%d"))
 
-status.data.date <- status.data %>%
-  mutate(weather_date=as.Date(weather_date, format = "%Y-%m-%d"))
 
-unique(air$AIRCRAFT_CLASS)
 air.date.sum <- air %>% 
   mutate(weather_date=as.Date(weather_date, format = "%Y-%m-%d")) %>% 
   group_by(FireID, weather_date) %>% 
@@ -87,14 +87,14 @@ is.Date(gis.data.date$weather_date)
 is.Date(weather$weather_date)
 
 join.gis.weather <-  left_join(gis.data.date, weather, by = c("FireID" = "FireID", "weather_date" = "weather_date"))
-join.status <- left_join(join.gis.weather, status.data.date, by = c("FireID" = "FireID", "weather_date" = "weather_date"))
-join.crew <- left_join(join.status, crew.date.sum, by = c("FireID" = "FireID", "weather_date" = "weather_date"))
+join.rh <- left_join(join.gis.weather, rh.lag, by = c("FireID" = "FireID", "weather_date" = "weather_date"))
+join.crew <- left_join(join.rh, crew.date.sum, by = c("FireID" = "FireID", "weather_date" = "weather_date"))
 join.air <- left_join(join.crew, air.date.sum, by = c("FireID" = "FireID", "weather_date" = "weather_date"))
 join.final <- left_join(join.air, equip.date, by = c("FireID" = "FireID", "weather_date" = "weather_date")) %>% 
   mutate(crewtotal=ifelse(is.na(crewtotal),0,crewtotal)) %>% 
-  mutate(airtotal=ifelse(is.na(airtotal),0,airtotal)) %>% 
-  mutate(equiptotal=ifelse(is.na(equiptotal),0,equiptotal)) %>% 
-  mutate(relativedays= FireDayActual/nogrowthday.x*100)
+  mutate(equiptotal=ifelse(is.na(equiptotal),0,equiptotal)) %>%   mutate(airtotal=ifelse(is.na(airtotal),0,airtotal)) %>% 
+
+  mutate(relativedays= FireDayActual/nogrowthday)
 
 #changins 0's to 1's so that row doesn't get dropped from SFA
 #Adding dummy variables for inputs
@@ -102,9 +102,20 @@ join.final <- join.final %>%
   mutate(Daily_Held_noneg = ifelse(Daily_Held_Perim <= 0,1,Daily_Held_Perim)) %>% 
   mutate(air_d = ifelse(airtotal == 0,1,0)) %>% 
   mutate(crew_d = ifelse(crewtotal == 0,1,0)) %>% 
-  mutate(equip_d = ifelse(equiptotal == 0,1,0))
+  mutate(equip_d = ifelse(equiptotal == 0,1,0)) %>% 
+  mutate(crew2 = crewtotal^2) %>% 
+  mutate(air2 = airtotal^2) %>% 
+  mutate(lnair = log(pmax(airtotal, air_d))) %>% 
+  mutate(lncrew = log(pmax( crewtotal, crew_d))) %>% 
+  mutate(lnair2 = (0.5*log(pmax(airtotal, air_d))^2)) %>% 
+  mutate(lncrew2 = (0.5*log(pmax( crewtotal, crew_d))^2)) %>% 
+  mutate(aircrew = (log(pmax(airtotal, air_d)))*(log(pmax( crewtotal, crew_d))))
 
-summary(join.final)
+#Changin RH value for HWF252 day 1 because missing value was generated (from AB ag)
+join.final[5, 31] = 79
+
+
+#summary(join.final)
 #Continue to Frontier_R_Code for analysis code
   
   
--- Frontier_R_Code.R
+++ Frontier_R_Code.R
@@ -7,7 +7,28 @@ require(plm)
 ################################################
 #          Dropping SWF fire due to no crew data
 join.final.noSWF <- join.final %>% filter(FireID != "SWF107")
-summary(join.final.noSWF)
+
+
+sumstat <- join.final.noSWF %>%
+  select(
+    `Fire Age` = FireDayActual,
+    `Held Perimeter` = Held_Perimeter, `Fire Size (ha)` = Area_ha,
+    `Crew` = crewtotal, `Air` = airtotal,
+    `RH` = rh_max,`Max Temperature` = temp_max,`Max Wind Speed` = agric_wind_max,
+    `% Held River` = perc_river, `Relative Age` = relativedays ) %>%
+summarise_each(funs(mean, sd, min, max)) %>%
+  # Move summary stats to columns
+  gather(key, value, everything()) %>% 
+  separate(key, into = c("variable", "stat"), sep = "_") %>%
+  spread(stat, value) %>%
+  # Set order of summary statistics 
+  select(variable, mean, sd, min, max) %>%
+  # Round all numeric variables to one decimal point
+  mutate_each(funs(round(., 1)), -variable)
+
+sumstat
+write.table(sumstat, file = "sumstats.xls", sep = ",", quote = FALSE, row.names = F)
+
 
 ################################################
 ################################################
@@ -15,8 +36,21 @@ summary(join.final.noSWF)
 library( "plm" )
 firePanel <- pdata.frame( join.final.noSWF, c( "FireID", "FireDayActual" ) )
 
-# require(ExPanDaR)
-# ExPanD(df = firePanel, cs_id = "FireID", ts_id = "FireDayActual")
+require(ExPanDaR)
+ExPanD(df = firePanel, cs_id = "FireID", ts_id = "FireDayActual")
+
+###############################################
+###############################################
+############ Create Correlation Plot
+# install.packages("corrplot")
+# library("corrplot")
+# library("corrgram")
+my.num.data <- join.final[, sapply(join.final, is.numeric)] %>%
+  select(Daily_Held_noneg, crew_d, air_d,
+         lnair, lncrew, rh_lag,  rh_max, agric_wind_max, agric_wind_avg, wind_speed, wind_speed_max,
+         perc_river, relativedays, Timber, FireDayActual, rain_total.y, rain_lag, dewpoint_max, temp_max)
+corrplot(corrgram(my.num.data), method = "ellipse")
+
 
 # Cobb-Douglas production frontier
 # cobbDouglas <- sfa( log( Daily_Held_noneg ) ~ log( crewtotal ) + log( airtotal ) + log( equiptotal ),
@@ -27,7 +61,7 @@ firePanel <- pdata.frame( join.final.noSWF, c( "FireID", "FireDayActual" ) )
 # that crew_d ¼ 1 if crew ¼ 0 and crew_d ¼ 0 if crew>0. Log of the resources variables
 # are created using: ln(crew) = ln(Max(crew, crew_d)).
 cobbDouglas <- sfa( log( Daily_Held_noneg ) ~ crew_d + air_d + log(pmax( crewtotal, crew_d))
-                      + log(pmax( crew2, crew_d)) + log(pmax(airtotal, air_d)),
+                     + log(pmax(airtotal, air_d)),
                       data = join.final.noSWF )
 summary(cobbDouglas)
 
@@ -35,7 +69,7 @@ summary(cobbDouglas)
 
 cobbDouglasFULL <- sfa(log( Daily_Held_noneg ) ~ crew_d + air_d + log(pmax( crewtotal, crew_d)) + 
                   log(pmax(airtotal, air_d)) + rh_lag + temp_max + agric_wind_max +  perc_river +
-                  Timber + relativedays , data = join.final.noSWF )
+                  Timber , data = join.final.noSWF )
 summary(cobbDouglasFULL) #remember! dummy vars have opposite interpretation, 1 = not used
 fitted( cobbDouglasFULL )
 join.final.noSWF$fittedcobb <- fitted( cobbDouglasFULL, asInData = TRUE )
@@ -78,21 +112,25 @@ efficiencies(fireTimeVar)
 
 # Technical Efficiency Effects Frontier (Battese & Coelli 1995)
 # (efficiency effects model with intercept)
-#trying to add crew2 variables!!!
+
 fireZ <- sfa(log( Daily_Held_noneg ) ~ crew_d + air_d  
              + lnair + lncrew
-             | rh_lag + temp_max + agric_wind_max + perc_river
-             + relativedays , data = firePanel )
+             | temp_max + agric_wind_max + rain_total.y, data = firePanel )
 summary(fireZ)
+
 efficiencies(fireZ)
 fitted( fireZ )
 firePanel$fittedZ <- fitted( fireZ, asInData = TRUE )
 plot(firePanel$fittedZ)
 
+
+
+
+
 fireZ.trans <- frontierQuad(log( Daily_Held_noneg ) ~ crew_d + air_d  
              + lnair + lncrew + lnair2 + lncrew2 + aircrew 
-             | rh_lag + temp_max + agric_wind_max + perc_river
-             + relativedays , data = firePanel )
+             | rh_lag + agric_wind_max + temp_max + rain_total.y
+             + perc_river + relativedays , data = firePanel )
 summary(fireZ.trans)
 efficiencies(fireZ.trans)
 fitted( fireZ.trans )
